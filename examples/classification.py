043e3a55d3415775d44b5cfbaf98a314f5c8be25
amanpreet692 <amanpreet.singh.x2@gmail.com>
2022-10-20 15:07:51 -0700

    classification eval

--- examples/classification.py
+++ examples/classification.py
@@ -5,8 +5,10 @@ sys.path.append('../')
 from evaluation.encoders import Model
 from evaluation.evaluator import SupervisedEvaluator, SupervisedTask
 
-#model = Model(base_checkpoint="allenai/specter")
-model = Model(base_checkpoint="../lightning_logs/full_run/scincl_ctrl/checkpoints/", task_id="[CLF]", use_ctrl_codes=True)
+# model = Model(base_checkpoint="allenai/specter")
+# model = Model(base_checkpoint="../lightning_logs/full_run/scincl_ctrl/checkpoints/", task_id="[CLF]", use_ctrl_codes=True)
+model = Model(base_checkpoint="malteos/scincl", variant="adapters",
+              adapters_load_from="../lightning_logs/full_run/scincl_adapters/checkpoints/", task_id="[CLF]")
 evaluator = SupervisedEvaluator(SupervisedTask.CLASSIFICATION, ("allenai/scirepeval", "biomimicry"),
                                 ("allenai/scirepeval_test", "biomimicry"), model, metrics=("binary",))
 

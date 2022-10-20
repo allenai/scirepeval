be596f15fe9dc8c87546d8d66dd446c007d42418
amanpreet692 <amanpreet.singh.x2@gmail.com>
2022-10-20 14:35:46 -0700

    classification eval

--- examples/classification.py
+++ examples/classification.py
@@ -7,7 +7,7 @@ from evaluation.evaluator import SupervisedEvaluator, SupervisedTask
 
 model = Model(base_checkpoint="allenai/specter")
 evaluator = SupervisedEvaluator(SupervisedTask.CLASSIFICATION, ("allenai/scirepeval", "biomimicry"),
-                                ("allenai/scirepeval_test", "biomimicry"), model)
+                                ("allenai/scirepeval_test", "biomimicry"), model, metrics=("binary",))
 
 embeddings = evaluator.generate_embeddings()
 

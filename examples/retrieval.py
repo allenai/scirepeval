57725f804f3eafe7c509cece45349e21fbfde91d
amanpreet692 <amanpreet.singh.x2@gmail.com>
2022-10-21 12:20:20 -0700

    reviewer matching and refactoring to have a separate method for metrics calculation

--- examples/retrieval.py
+++ examples/retrieval.py
@@ -4,7 +4,7 @@ sys.path.append('../')
 
 from evaluation.encoders import Model
 from evaluation.evaluator import IREvaluator
-
+from reviewer_matching import ReviewerMatchingEvaluator
 # model = Model(base_checkpoint="allenai/specter")
 model = Model(base_checkpoint="../lightning_logs/full_run/scincl_ctrl/checkpoints/",
               task_id={"query": "[QRY]", "candidate": "[SAL]"},
@@ -18,9 +18,10 @@ model = Model(base_checkpoint="../lightning_logs/full_run/scincl_ctrl/checkpoint
 #
 # evaluator.evaluate(embeddings)
 
-evaluator = IREvaluator("feeds_1", ("allenai/scirepeval", "feeds_title"), ("allenai/scirepeval_test", "feeds_title"),
-                        model,
-                        metrics=("map", "ndcg",))
+# evaluator = IREvaluator("feeds_1", ("allenai/scirepeval", "feeds_title"), ("allenai/scirepeval_test", "feeds_title"),
+#                         model, metrics=("map", "ndcg",))
+evaluator = ReviewerMatchingEvaluator("paper reviewer evaluation", ("allenai/scirepeval", "paper_reviewer_matching"), ("allenai/scirepeval_test", "paper_reviewer_matching"),
+                         ("allenai/scirepeval_test", "reviewers"), model, metrics=("map", "ndcg",))
 
 embeddings = evaluator.generate_embeddings()
 

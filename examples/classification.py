78841c6ee6e21c0d71e56376d5472c6ea254c870
amanpreet692 <amanpreet.singh.x2@gmail.com>
2022-10-21 14:16:41 -0700

    reviewer matching fix

--- examples/classification.py
+++ examples/classification.py
@@ -5,12 +5,12 @@ sys.path.append('../')
 from evaluation.encoders import Model
 from evaluation.evaluator import SupervisedEvaluator, SupervisedTask
 
-# model = Model(base_checkpoint="allenai/specter")
+model = Model(base_checkpoint="allenai/specter")
 # model = Model(base_checkpoint="../lightning_logs/full_run/scincl_ctrl/checkpoints/", task_id="[CLF]", use_ctrl_codes=True)
-model = Model(base_checkpoint="malteos/scincl", variant="adapters",
-              adapters_load_from="../lightning_logs/full_run/scincl_adapters/checkpoints/", task_id="[CLF]")
+# model = Model(base_checkpoint="malteos/scincl", variant="adapters",
+#               adapters_load_from="../lightning_logs/full_run/scincl_adapters/checkpoints/", task_id="[CLF]")
 evaluator = SupervisedEvaluator("biomimicry", SupervisedTask.CLASSIFICATION, ("allenai/scirepeval", "biomimicry"),
-                                ("allenai/scirepeval_test", "biomimicry"), model, metrics=("macro",))
+                                ("allenai/scirepeval_test", "biomimicry"), model, metrics=("f1",))
 
 embeddings = evaluator.generate_embeddings()
 

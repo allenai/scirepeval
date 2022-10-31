7f83bd68df83615ea2d8327f13473b8682133e6a
amanpreet692 <amanpreet.singh.x2@gmail.com>
2022-10-31 11:22:58 -0700

    testing fixes

--- examples/regression.py
+++ examples/regression.py
@@ -5,8 +5,8 @@ sys.path.append('../')
 from evaluation.encoders import Model
 from evaluation.evaluator import SupervisedEvaluator, SupervisedTask
 
-# model = Model(base_checkpoint="allenai/specter")
-model = Model(base_checkpoint="../lightning_logs/full_run/scincl_ctrl/checkpoints/", task_id="[RGN]", use_ctrl_codes=True)
+model = Model(base_checkpoint="allenai/specter")
+# model = Model(base_checkpoint="../lightning_logs/full_run/scincl_ctrl/checkpoints/", task_id="[RGN]", use_ctrl_codes=True)
 # model = Model(base_checkpoint="malteos/scincl", variant="adapters",
 #               adapters_load_from="../lightning_logs/full_run/scincl_adapters/checkpoints/", task_id="[CLF]")
 evaluator = SupervisedEvaluator("max hIndex", SupervisedTask.REGRESSION, ("allenai/scirepeval", "peer_review_score_hIndex"),

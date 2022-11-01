24cacdefa02a854029eb97b307582d5503ac94e0
amanpreet692 <amanpreet.singh.x2@gmail.com>
2022-11-01 14:45:48 -0700

    encoder eval fix

--- examples/retrieval.py
+++ examples/retrieval.py
@@ -1,5 +1,7 @@
 import sys
 
+from evaluator import IREvaluator
+
 sys.path.append('../')
 
 from evaluation.encoders import Model
@@ -16,8 +18,8 @@ model = Model(base_checkpoint="malteos/scincl", variant="adapters",
               adapters_load_from="../../../phantasm/phantasm_new/lightning_logs/full_run/scincl_adapters/checkpoints/",
               task_id="[PRX]", all_tasks=["[PRX]"])
 
-# evaluator = IREvaluator("feeds_1", ("allenai/scirepeval", "feeds_1"), ("allenai/scirepeval_test", "feeds_1"), model,
-#                         metrics=("map", "ndcg",))
+evaluator = IREvaluator("feeds_1", ("allenai/scirepeval", "feeds_1"), ("allenai/scirepeval_test", "feeds_1"), model,
+                        metrics=("map", "ndcg",))
 #
 # embeddings = evaluator.generate_embeddings()
 #
@@ -25,9 +27,9 @@ model = Model(base_checkpoint="malteos/scincl", variant="adapters",
 
 # evaluator = IREvaluator("feeds_1", ("allenai/scirepeval", "feeds_title"), ("allenai/scirepeval_test", "feeds_title"),
 #                         model, metrics=("map", "ndcg",))
-evaluator = ReviewerMatchingEvaluator("paper reviewer evaluation", ("allenai/scirepeval", "paper_reviewer_matching"),
-                                      ("allenai/scirepeval_test", "paper_reviewer_matching"),
-                                      ("allenai/scirepeval_test", "reviewers"), model, metrics=("map", "ndcg",))
+# evaluator = ReviewerMatchingEvaluator("paper reviewer evaluation", ("allenai/scirepeval", "paper_reviewer_matching"),
+#                                       ("allenai/scirepeval_test", "paper_reviewer_matching"),
+#                                       ("allenai/scirepeval_test", "reviewers"), model, metrics=("map", "ndcg",))
 
 embeddings = evaluator.generate_embeddings()
 

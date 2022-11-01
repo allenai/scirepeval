ed7fd6bc875bb963986ba8184f00608eb90943de
amanpreet692 <amanpreet.singh.x2@gmail.com>
2022-11-01 14:44:42 -0700

    encoder eval fix

--- examples/retrieval.py
+++ examples/retrieval.py
@@ -6,15 +6,15 @@ from evaluation.encoders import Model
 from reviewer_matching import ReviewerMatchingEvaluator
 
 # default no control codes
-model = Model(base_checkpoint="allenai/specter")
+# model = Model(base_checkpoint="allenai/specter")
 
 # default control codes
 # model = Model(base_checkpoint="../lightning_logs/full_run/scincl_ctrl/checkpoints/", task_id="[PRX]", use_ctrl_codes=True)
 
-# single adapters
-# model = Model(base_checkpoint="malteos/scincl", variant="adapters",
-#               adapters_load_from="../lightning_logs/full_run/scincl_adapters/checkpoints/", task_id="[{RX}]")
 
+model = Model(base_checkpoint="malteos/scincl", variant="adapters",
+              adapters_load_from="../../../phantasm/phantasm_new/lightning_logs/full_run/scincl_adapters/checkpoints/",
+              task_id="[PRX]", all_tasks=["[PRX]"])
 
 # evaluator = IREvaluator("feeds_1", ("allenai/scirepeval", "feeds_1"), ("allenai/scirepeval_test", "feeds_1"), model,
 #                         metrics=("map", "ndcg",))

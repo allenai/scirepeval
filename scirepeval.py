12729669b6bb4f28d7b9deb1920d0da6e56063d6
amanpreet692 <amanpreet.singh.x2@gmail.com>
2022-11-02 11:11:35 -0700

    adapters hf checkpoint

--- scirepeval.py
+++ scirepeval.py
@@ -116,11 +116,12 @@ if __name__ == "__main__":
     parser.add_argument('--model', '--m', help='HuggingFace model to be used')
     parser.add_argument('--ctrl-tokens', action='store_true', default=False, help='use control codes for tasks')
     parser.add_argument('--adapters-dir', help='path to the adapter checkpoints', default=None)
+    parser.add_argument('--adapters-chkpt', help='hf adapter names keyed on tasks', default=None, type=json.loads)
     parser.add_argument('--output', help="path to the output file", default="scirepeval_results.json")
 
     args = parser.parse_args()
-
-    model = Model(variant=args.mtype, base_checkpoint=args.model, adapters_load_from=args.adapters_dir,
+    adapters_load_from = args.adapters_dir if args.adapters_dir else args.adapters_chkpt
+    model = Model(variant=args.mtype, base_checkpoint=args.model, adapters_load_from=adapters_load_from,
                   use_ctrl_codes=args.ctrl_tokens,
                   task_id="", all_tasks=["[CLF]", "[QRY]", "[RGN]", "[PRX]"])
     evaluator = SciRepEval(tasks_config=args.tasks_confg)

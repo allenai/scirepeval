471e73760a7cbaf2424e8a8ecb1838589c98ad82
dcdowney <dougd@allenai.org>
2024-05-14 19:43:06 -0700

    bug fixes

--- evaluation/arctic_embedding.py
+++ evaluation/arctic_embedding.py
@@ -16,8 +16,8 @@ class ArcticModel:
     def __call__(self, batch, batch_ids=None):
         if type(self.task_id) == dict or self.task_id not in self.retrieval_tasks:
             batch = [f"{instr_format if [b[1]] == 'q' else ''}{batch[i]}" for i, b in enumerate(batch_ids)]
-        self.tokenizer(batch, padding=True, truncation=True, return_tensors='pt', max_length=512)
+        inputs = self.tokenizer(batch, padding=True, truncation=True, return_tensors='pt', max_length=512)
         with torch.no_grad():
-            batch_embed = self.encoder(**batch)[0][:, 0]
-            batch_embed = torch.functional.normalize(batch_embed, p=2, dim=1)
+            batch_embed = self.encoder(**inputs).last_hidden_state[:, 0]
+            batch_embed =  torch.nn.functional.normalize(batch_embed, p=2, dim=1)
         return batch_embed
